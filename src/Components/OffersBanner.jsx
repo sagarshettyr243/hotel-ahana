409f9cad8ec42c176004e0c93884ae5ca4c8f5a9
root <root@DESKTOP-05FMOSU>
2025-01-14 22:54:21 +0530

    Updated the file

--- src/Components/OffersBanner.jsx
+++ src/Components/OffersBanner.jsx
@@ -21,7 +21,7 @@ function OffersBanner() {
 
             <div className='w-75 mt-5'>
                 
-                <h4 className='fw-bolder'>What's on your mind Today?</h4>
+                <h4 className='fw-bolder'>What's on your mind Today, Sagar Shetty?</h4>
                 <div className='d-flex align-items-center' style={{overflowY:"scroll",gap:"19px"}}>
                     <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png" alt="Biriyani" />
                     <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667626/PC_Creative%20refresh/South_Indian_4.png" alt="South Indian" />
